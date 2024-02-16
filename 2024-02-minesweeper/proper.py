915fc7e9b87c68597fde8ef3c6ad373b9290e387
Pixirora <rina@pixirora.dev>
2024-02-15 23:24:15 -0500

    Add givens. No complete solution yet

--- 2024-02-minesweeper/proper.py
+++ 2024-02-minesweeper/proper.py
@@ -1,5 +1,15 @@
+from typing import NamedTuple
+
+SPEED_OF_SOUND: float = 0.343
+
 SoundImpactData = tuple[int, int, float]
 
+class Circle(NamedTuple):
+    x: int
+    y: int
+    radius: float
 
 def find(s1: SoundImpactData, s2: SoundImpactData, s3: SoundImpactData) -> tuple[int, int]:
-    ...
+    c1, c2, c3 = (Circle(s[0], s[1], s[0] * SPEED_OF_SOUND) for s in (s1, s2, s3))
+
+    return (0, 0)
