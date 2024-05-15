41199c194ca81e38f612350943bae01c97732b08
Pixirora <rina@pixirora.dev>
2024-05-14 21:55:22 -0400

    solve for finding the optimal seats as well

--- 2024-05-going_to_the_movies/proper.py
+++ 2024-05-going_to_the_movies/proper.py
@@ -1,12 +1,16 @@
-import math
 from dataclasses import dataclass
-from typing import NamedTuple
+from typing import Generator, NamedTuple, Self
 
 
 class Vec(NamedTuple):
     x: int
     y: int
 
+    def midpoint(self, other: Self) -> Self:
+        return self.__class__((self.x + other.x) // 2, (self.y + other.y) // 2)
+
+    def dist(self, other: Self) -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
 
 @dataclass(slots=True)
 class MovieTheatre:
@@ -15,30 +19,34 @@ class MovieTheatre:
     def __init__(self, seats: list[list[int]]) -> None:
         self.seats = seats
 
-    def valid_positions(self, number_of_friends: int) -> list[Vec]:
-        valid_pos: list[Vec] = []
+    @property
+    def size(self) -> Vec:
+        return Vec(len(self.seats[0]), len(self.seats))
 
+    @property
+    def center(self) -> Vec:
+        return self.size.midpoint(Vec(0, 0))
+
+    def valid_positions(self, number_of_friends: int) -> Generator[Vec, None, None]:
         for y, row in enumerate(self.seats):
             untaken_seats: int = 0
 
-            for x, seat_open in enumerate(row):
-                if not seat_open:
+            for x, seat_taken in enumerate(row):
+                if not seat_taken:
                     untaken_seats += 1
 
                     if untaken_seats >= number_of_friends:
-                        seat_group_pos: Vec = Vec(x - math.ceil(untaken_seats / 2), y)
-                        valid_pos.append(seat_group_pos)
+                        yield Vec(x - round(untaken_seats / 2), y)
                 else:
                     untaken_seats = 0
-
-        return valid_pos
-
 
 def find_seats(seats: list[list[int]], n: int) -> int:
     theatre: MovieTheatre = MovieTheatre(seats)
 
-    return len(theatre.valid_positions(n))
+    return len(list(theatre.valid_positions(n)))
 
 
-def optimal_seats(seats: list[list[int]], n: int) -> int:
-    ...
+def optimal_seats(seats: list[list[int]], n: int) -> tuple[int, int]:
+    theatre: MovieTheatre = MovieTheatre(seats)
+
+    return min(theatre.valid_positions(n), key=theatre.center.dist)
