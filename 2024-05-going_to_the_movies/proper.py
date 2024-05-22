cba9d586085ab355e2343132f6d5ed164cf4f6d9
Pixirora <rina@pixirora.dev>
2024-05-21 22:14:15 -0400

    Clean up the proper implementation to be actually proper

--- 2024-05-going_to_the_movies/proper.py
+++ 2024-05-going_to_the_movies/proper.py
@@ -1,44 +1,46 @@
 from dataclasses import dataclass
-from typing import Generator, NamedTuple, Self
+from typing import Iterator, NamedTuple, Self
 
 
-class Vec(NamedTuple):
+class TaxicabVec(NamedTuple):
     x: int
     y: int
 
     def midpoint(self, other: Self) -> Self:
-        return self.__class__((self.x + other.x) // 2, (self.y + other.y) // 2)
+        return type(self)((self.x + other.x) // 2, (self.y + other.y) // 2)
 
     def dist(self, other: Self) -> int:
         return abs(self.x - other.x) + abs(self.y - other.y)
+
 
 @dataclass(slots=True)
 class MovieTheatre:
     seats: list[list[int]]
 
-    def __init__(self, seats: list[list[int]]) -> None:
-        self.seats = seats
+    @property
+    def size(self) -> TaxicabVec:
+        row_count: int = len(self.seats)
+        seats_per_row: int = len(self.seats[0]) if row_count != 0 else 0
+
+        return TaxicabVec(seats_per_row, row_count)
 
     @property
-    def size(self) -> Vec:
-        return Vec(len(self.seats[0]), len(self.seats))
+    def center(self) -> TaxicabVec:
+        return self.size.midpoint(TaxicabVec(0, 0))
 
-    @property
-    def center(self) -> Vec:
-        return self.size.midpoint(Vec(0, 0))
-
-    def valid_positions(self, number_of_friends: int) -> Generator[Vec, None, None]:
+    def valid_positions(self, group_size: int) -> Iterator[TaxicabVec]:
         for y, row in enumerate(self.seats):
             untaken_seats: int = 0
 
             for x, seat_taken in enumerate(row):
-                if not seat_taken:
+                if seat_taken:
+                    untaken_seats = 0
+                else:
                     untaken_seats += 1
 
-                    if untaken_seats >= number_of_friends:
-                        yield Vec(x - round(untaken_seats / 2), y)
-                else:
-                    untaken_seats = 0
+                if untaken_seats >= group_size:
+                    yield TaxicabVec(x - round(untaken_seats / 2), y)
+
 
 def find_seats(seats: list[list[int]], n: int) -> int:
     theatre: MovieTheatre = MovieTheatre(seats)
