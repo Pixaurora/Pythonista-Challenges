f8c80628aae31fa5b04bcf62bc20ad37c5f6e4c7
Pixirora <rina@pixirora.dev>
2024-07-30 22:27:35 -0400

    Add givens

---
+++ 2024-07-bank_breakout/__init__.py
@@ -0,0 +1,6 @@
+from . import quick_and_dirty
+
+
+tested_function = quick_and_dirty.decipher
+
+test_cases = [((["1233", "1234", "1243"],), "3777"), ((["1235", "2346", "3457", "9999", "3333", "5559", "4567"],), "7390")]
